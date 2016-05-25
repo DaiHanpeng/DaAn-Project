d7384bf5a9ba11c8001acf45f8ea24db5bcbc4e5
DaiHanpeng <AndySunshines@126.com>
2016-05-25 23:22:12 +0800

    RpcInterface.py changed: implementation of rpc server.

--- Part II/RpcInterface/RpcInterface.py
+++ Part II/RpcInterface/RpcInterface.py
@@ -1,13 +1,51 @@
 import xmlrpclib
-import SimpleXMLRPCServer
+from SimpleXMLRPCServer import SimpleXMLRPCServer
 
-from DBInterface.DBInterface import DBInterface
+from DBInterface.SqliteInterface import SqliteInterface
+from DaAnInterface.DaAnInterface import DaAnInterface
 
-SimpleXMLRPCServer.register_function()
 
-class RpcServerInterface(SimpleXMLRPCServer):
+class MessageHandler():
     """
-    rpc server Interface.
     """
-    def __init__():
+    def __init__(self):
         pass
+
+    def reagent_handler(self):
+        print 'reagent handler is triggered...'
+        with SqliteInterface() as db_interface:
+            print 'getting reagent start...'
+            try:
+                reagents = db_interface.fetch_unsent_items_from_InsReagentInfo()
+                if reagents:
+                    ##
+                    print 'get unsent ins reagent info records,'
+                    for reagent in reagents:
+                        if isinstance(reagent,ins_reagent_info):
+                            print reagent
+                    ## reagent info processing...
+                    daan_interface = DaAnInterface()
+                    daan_interface.send_reagent_info(reagents)
+                    ##
+                    db_interface.set_InsReagentInfos_as_sent(reagents)
+            except Exception as ex:
+                print ex
+
+    def order_result_handler(self):
+        pass
+
+    def calibration_handler(self):
+        pass
+
+    def control_handler(self):
+        pass
+
+def test():
+    server = SimpleXMLRPCServer(("localhost", 8000))
+    server.register_introspection_functions()
+    server.register_instance(MessageHandler())
+    server.serve_forever()
+    input('should not be here ...')
+
+if __name__  == '__main__':
+    test()

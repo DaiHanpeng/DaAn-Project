2bb4f0588ff06d67ddc58bef6a72d5f4f62c6004
DaiHanpeng <AndySunshines@126.com>
2016-05-25 23:16:34 +0800

    add RpcClient for Part I.

---
+++ Part I/RpcInterface/RpcClient.py
@@ -0,0 +1,31 @@
+import xmlrpclib
+
+class RpcClient():
+    """
+    rpc framework client.
+    """
+    def __init__(self):
+        self.server_proxy = xmlrpclib.ServerProxy('http://localhost:8000')
+
+    def fire_reagent_notification(self):
+        try:
+            self.server_proxy.reagent_handler()
+        except Exception as ex:
+            print ex
+
+    def fire_order_result_notification(self):
+        pass
+
+    def fire_calibration_notification(self):
+        pass
+
+    def fire_control_notification(self):
+        pass
+
+
+def test():
+    rpc_client = RpcClient()
+    rpc_client.fire_reagent_notification()
+
+if __name__ == '__main__':
+    test()

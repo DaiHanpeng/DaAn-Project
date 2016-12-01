7c510ecd470373f095256fcc1773410b67f007a3
DaiHanpeng <AndySunshines@126.com>
2016-12-01 10:37:00 +0800

    bug fixed for Control: 1. add module name as 'Control' 2. add timestamp every time the record is scanned, to avoid duplicated records sent to DaAn Cloud. 3. spelling error change, 'REAULT' to 'RESULT'.

--- Part I/ControlParser/ControlParser.py
+++ Part I/ControlParser/ControlParser.py
@@ -1,4 +1,6 @@
 from GetLatestFile.GetLatestFile import GetLatestFile
+from DatabaseInterface.MySqlTimestampInterface import MYSQL_DB_SCHEMA,TableTimestampInterface
+import datetime
 
 class ControlInfo():
     """
@@ -26,19 +28,29 @@ class ControlParser():
     """
     parser for control data from printed file.
     """
+    MODULE_NAME = r'Control'
 
-    PARSING_MODE = {'OFF':0, 'SECTION':1, 'HEAD':2, 'REAULT':3}
+    PARSING_MODE = {'OFF':0, 'SECTION':1, 'HEAD':2, 'RESULT':3}
 
     SECTION_LINE_MIN_LENGTH = 110
     ISE_HEAD_LINE = r'Test Name      Conc.  Unit        Mark            SAMPLE    BUFFER  Electrode/Refrence Lot  UserCode'
     NORMAL_HEAD_LINE = r'Test Name      Conc.  Unit        Mark            ABS-RB       ABS  R1 LOT#  R2 LOT#  UserCode'
 
-
     def __init__(self):
         self.qc_list = []
+        try:
+            db_interface = TableTimestampInterface()
+            db_interface.db_connect_initialize(MYSQL_DB_SCHEMA)
+            self.last_updated_timestamp = db_interface.get_table_last_updated_timestamp(self.MODULE_NAME)
+            print self.MODULE_NAME,' last updated timestamp: ', self.last_updated_timestamp
+        except Exception as e:
+            print 'db timestamp initialize failed!', e
+        finally:
+            db_interface.db_disconnect()
 
     def extract_qc_info(self,qc_file_path):
         self.qc_list = []
+        last_updated_timestamp = self.last_updated_timestamp
 
         lates_control_file = GetLatestFile.get_latest_file(qc_file_path,'Control_','.TXT')
 
@@ -70,22 +82,36 @@ class ControlParser():
                     elif ControlParser.PARSING_MODE['HEAD'] == parsing_mode:
                         if ControlParser.ISE_HEAD_LINE == line.strip() or\
                             ControlParser.NORMAL_HEAD_LINE == line.strip():
-                            parsing_mode = ControlParser.PARSING_MODE['REAULT']
+                            parsing_mode = ControlParser.PARSING_MODE['RESULT']
                     #RESULT mode
-                    elif ControlParser.PARSING_MODE['REAULT'] == parsing_mode:
+                    elif ControlParser.PARSING_MODE['RESULT'] == parsing_mode:
                         if qc_lot and date_time:
-                            test = line[0:10].strip()
-                            value = line[12:21].strip()
-                            unit = line[22:33].strip()
-                            abs_rb = line[46:57].strip()
-                            abs = line[58:67].strip()
+                            f_date_time = datetime.datetime.strptime(date_time,'%d %b %Y %H:%M:%S')
+                            if f_date_time > last_updated_timestamp:
+                                test = line[0:10].strip()
+                                value = line[12:21].strip()
+                                unit = line[22:33].strip()
+                                abs_rb = line[46:57].strip()
+                                abs = line[58:67].strip()
 
-                            qc_info = ControlInfo(qc_lot,date_time,test,value,unit,abs_rb,abs)
-                            self.qc_list.append(qc_info)
+                                qc_info = ControlInfo(qc_lot,date_time,test,value,unit,abs_rb,abs)
+                                self.qc_list.append(qc_info)
+                                if f_date_time > self.last_updated_timestamp:
+                                    self.last_updated_timestamp = f_date_time
                 else:
                     qc_lot = ''
                     date_time = ''
                     parsing_mode = ControlParser.PARSING_MODE['SECTION']
+
+        if self.last_updated_timestamp > last_updated_timestamp:
+            try:
+                db_interface = TableTimestampInterface()
+                db_interface.db_connect_initialize(MYSQL_DB_SCHEMA)
+                db_interface.update_table_timestamp(self.MODULE_NAME,self.last_updated_timestamp)
+            except Exception as e:
+                print 'db update timestamp failed!', e
+            finally:
+                db_interface.db_disconnect()
 
     def __repr__(self):
         return 'control info list:\n'+\
@@ -93,7 +119,7 @@ class ControlParser():
 
 def test():
     qc_parser = ControlParser()
-    qc_path = r'D:\01_Automation\23_Experiential_Conclusions_2016\05_DaAn\Reports\DaAn_Reports\Reports'
+    qc_path = r'.'
     qc_parser.extract_qc_info(qc_path)
     print qc_parser
 
